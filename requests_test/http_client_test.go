e407b04d9486ac652560a0ea93322bda096e81b3
parvez <parvez@yellowmessenger.com>
2020-09-08 15:51:49 +0530

    added new test cases

--- requests_test/http_client_test.go
+++ requests_test/http_client_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 // global http client
-var client *
+var client *requests.Client
 
 // creating a http client with default settings
 func TestCreateClient(t *testing.T) {
@@ -16,7 +16,7 @@ func TestCreateClient(t *testing.T) {
 	headers.Set("Content-Type", "application/json")
 	gOptions := requests.GlobalOptions{
 		Timeout:  30 * time.Second,
-		BasePath: "https://app.yellowmessenger.com",
+		BasePath: "https://www.random.org/",
 		Headers:  headers,
 	}
 	client = requests.NewClient(gOptions)
@@ -24,10 +24,33 @@ func TestCreateClient(t *testing.T) {
 }
 
 // initializing the request with custom options
-func TestDoGetRequest(t *testing.T) {
+func TestGetRequest(t *testing.T) {
 	options := requests.Options{
-		Url: "/api/login",
+		Url: "/quick-pick/",
 		Method: "GET",
+	}
+	err := client.NewRequest(options)
+	if err != nil{
+		t.Fatalf("failed to create initailize the request object - %v", err)
+	}
+	res, err := client.Send()
+	if err != nil{
+		t.Fatalf("failed to make the get request - %v", err)
+	}
+	statusCode := res.GetStatusCode()
+	_, err = res.GetBody()
+	if err != nil{
+		t.Fatalf("failed to get body from response - %v", err)
+	}
+	t.Logf("success in making the api call with status code - %d", statusCode)
+}
+
+// testing get request with query parameters
+func TestQueryGetRequest(t *testing.T) {
+	options := requests.Options{
+		Url: "/quick-pick/",
+		Method: "GET",
+		Query: map[string]string{"tickets": "2", "lottery": "5x69.1x26"},
 	}
 	err := client.NewRequest(options)
 	if err != nil{
